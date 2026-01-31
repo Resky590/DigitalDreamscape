0b5fb468c6c81514e4fb67a6c8b796dc53ffcecf
Resky590 <Reskyk801@gmail.com>
2026-01-31 21:37:37 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,26 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
+
+// --- IMPORT HALAMAN ---
 import Home from "@/pages/home";
+import Gallery from "@/pages/gallery";
+import About from "@/pages/about";
+import Editor from "@/pages/editor"; // <-- PENTING: Import halaman Editor
 
 function Router() {
   return (
     <Switch>
+      {/* Daftar Rute Website */}
       <Route path="/" component={Home} />
+      <Route path="/gallery" component={Gallery} />
+      <Route path="/about" component={About} />
+      
+      {/* Rute Baru untuk Editor */}
+      <Route path="/editor" component={Editor} /> 
+      
+      {/* Halaman 404 jika link tidak ditemukan */}
       <Route component={NotFound} />
     </Switch>
   );
@@ -18,10 +30,8 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
+      <Router />
+      <Toaster />
     </QueryClientProvider>
   );
 }
